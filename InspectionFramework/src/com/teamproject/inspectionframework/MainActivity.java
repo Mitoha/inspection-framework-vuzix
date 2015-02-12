cf5e3978bccccb16706eaf3074a5ea870ee1ad46
Mitoha <michaeltobiashartl@googlemail.com>
2015-02-12 16:58:34 +0100

    Adding test screens

--- InspectionFramework/src/com/teamproject/inspectionframework/MainActivity.java
+++ InspectionFramework/src/com/teamproject/inspectionframework/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+//import com.vuzix.speech.VoiceControl;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.button1);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+        	
+            public void onClick(View v) {
+                setContentView(R.layout.login_screen);
+            } 
+        });
+        
+
+        //myVoiceControl voice = null;
+
     }
 
 
