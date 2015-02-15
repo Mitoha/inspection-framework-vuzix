39381c569ae4c7efa5192b26ed5c95d8bd1b3bf7
Mitoha <michaeltobiashartl@googlemail.com>
2015-02-16 00:19:11 +0100

    Fixed files

--- InspectionFramework/src/com/teamproject/inspectionframework/MainActivity.java
+++ InspectionFramework/src/com/teamproject/inspectionframework/MainActivity.java
@@ -1,12 +1,10 @@
 package com.teamproject.inspectionframework;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-//import com.vuzix.speech.VoiceControl;
 
 public class MainActivity extends Activity {
 
@@ -14,19 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        final Button button = (Button) findViewById(R.id.button1);
-        
-        button.setOnClickListener(new View.OnClickListener() {
-        	
-            public void onClick(View v) {
-                setContentView(R.layout.login_screen);
-            } 
-        });
-        
-
-        //myVoiceControl voice = null;
-
+        ActionBar actionBar = getActionBar();
+        actionBar.setSubtitle("User Login");
     }
 
 
